0bb2b155a6690e5b1d187c6e51f7bac44d7666d9
Matheus Soares <m.soares.ms@outlook.com>
2017-10-04 11:11:05 -0400

    remoção de arquivos desnecessários

--- main.cpp
+++ main.cpp
@@ -20,20 +20,3 @@ int main() {
     return 0;
 }
 //#endif
-
-#ifdef COMPILA
-int main(){
-    Item a(1);
-    Item b(2);
-    Item c(3);
-
-    Lista<Item> teste;
-
-    teste.insere(a);
-    teste.insere(b);
-    teste.insere(c);
-
-    teste.mostrar();
-
-}   
-#endif
