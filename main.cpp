8a7e4693189b163ebc937df6bfe7422a17cefa48
Matheus Soares <m.soares.ms@outlook.com>
2017-10-03 22:22:34 -0400

    ...

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 //#ifdef COMPILA
 int main() {
-    Grafo g(5);
+    Grafo g(2);
 
     g.inserirAresta(1,2);
-    g.inserirAresta(2,3);
-    g.inserirAresta(3,4);
-    g.inserirAresta(4,5);
-    g.inserirAresta(5,1);
-    g.inserirAresta(5,2);
-    g.inserirAresta(2,4);
+//    g.inserirAresta(2,3);
+//    g.inserirAresta(3,4);
+//    g.inserirAresta(4,5);
+//    g.inserirAresta(5,1);
+//    g.inserirAresta(5,2);
+//    g.inserirAresta(2,4);
 
     g.mostrar();
 
